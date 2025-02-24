2f8bb9b3547a5f6709c6276f359022389cdd5f17
MichaelToast <michaelp86788@gmail.com>
2025-02-23 21:06:15 -0600

    Added Getters to Characters and attack functionality to Wizards

--- src/edu/mu/middleearth/MiddleEarthCharacter.java
+++ src/edu/mu/middleearth/MiddleEarthCharacter.java
@@ -15,5 +15,38 @@ public abstract class MiddleEarthCharacter {
 		this.power = power;
 	}
 	
+	public void displayInfo() {
+		System.out.println("NAME: " + name + ", HEALTH: " + health + ", POWER: " + power);
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getHealth() {
+		return health;
+	}
+	public void setHealth(double health) {
+		this.health = health;
+	}
+	public double getPower() {
+		return power;
+	}
+	public void setPower(double power) {
+		this.power = power;
+	}
+	
+	// ASK IF THIS IS OK
+	protected void takeDamage(double damageTaken) {
+		health = health - damageTaken;
+		if (health < 0) {
+			health = 0;
+		}
+		
+	}
+	
 
 }
--- src/edu/mu/middleearth/Wizard.java
+++ src/edu/mu/middleearth/Wizard.java
@@ -4,13 +4,30 @@ public class Wizard extends MiddleEarthCharacter{
 
 	public Wizard(String name, double health, double power) {
 		super(name, health, power);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
+	/**
+	 * Remove health from target if against valid type
+	 * @param target Character to attack
+	 * @return boolean detailing weather the attack failed or not 
+	 * */
 	boolean attack(MiddleEarthCharacter target) {
-		// TODO Auto-generated method stub
-		return false;
+		if (target.getRace().equals(this.getRace())) {
+			// Ineffective against same type as self 
+			return false;
+		}
+		else if (target.getRace().equals("Human")) {
+			// Ineffective against Human 
+			return false;
+		}
+		else if (target.getRace().equals("Dwarf")) {
+			target.takeDamage(this.getPower() * 1.5);
+			return true;
+		}
+		
+		target.takeDamage(this.getPower());
+		return true;
 	}
 
 	@Override
