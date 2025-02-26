e22204024776f9ce8f81c54e02816cf737c8c8bf
noahmcallister04 <116388091+noahmcallister04@users.noreply.github.com>
2025-02-26 17:34:16 -0600

    Added functionality to deleteCharacter in CharacterManager

--- src/edu/mu/middleearth/managment/CharacterManager.java
+++ src/edu/mu/middleearth/managment/CharacterManager.java
@@ -22,6 +22,35 @@ public class CharacterManager {
 		return false; 
 	}
 	
+	boolean deleteCharacter(MiddleEarthCharacter character) {
+		if (character == null) {
+			return false;
+		}
+		// Finds character in the array
+		int index = -1;
+		for (int i = 0; i < size; i++) {
+			if (characters[i].equals(character)) {
+				index = i;
+				break;
+			}
+		}
+		// Character not found, no action taken
+		if (index == -1) {
+			return false;
+		}
+		
+		// Shifts elements after the character to the left
+		for (int i = index; i < size - 1; i++) {
+			characters[i] = characters[i + 1];
+		}
+		
+		// Ensures no duplicates after an element is deleted
+		characters[size - 1] = null;
+		size--;
+		
+		return true;
+	}
+	
 	void displayAllCharacters() {
 		// Display all characters
 	}
